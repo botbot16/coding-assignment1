d923b42ef1ecda8238b66120a2ef640b4964941b
botbot16 <75625596+botbot16@users.noreply.github.com>
2024-01-23 08:31:09 +0200

    Update RotateArray.java

--- RotateArray.java
+++ RotateArray.java
@@ -40,35 +40,50 @@ public class RotateArray {
         for (int i = k; i < arr.length - k; i++) {
             arr[i] = arr[i + k];
         }
-        // move the elements from temp to arr (left to right).
+        // move the elements from temp to the end of arr.
         for (int i = arr.length - k; i < arr.length; i++) {
             arr[i] = temp[i - (arr.length - k)];
         }
     }
 
     public static void main(String[] args) {
-        int[] arr = new int[5];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = i;
+        int[] original_arr = new int[7];
+        for (int i = 0; i < original_arr.length; i++) {
+            original_arr[i] = i + 1;
         }
-        System.out.println(Arrays.toString(arr));
-        RotateLeftInPlace(arr, 7);
-        System.out.println(Arrays.toString(arr));
-        assert arr[0] == 2;
-        assert arr[1] == 3;
-        assert arr[2] == 4;
-        assert arr[3] == 0;
-        assert arr[4] == 1;
+        int[] arr1 = original_arr.clone();
+        System.out.println(Arrays.toString(arr1));
+        RotateLeftInPlace(arr1, 2);
+        System.out.println(Arrays.toString(arr1));
+        assert arr1[0] == 3;
+        assert arr1[1] == 4;
+        assert arr1[2] == 5;
+        assert arr1[3] == 6;
+        assert arr1[4] == 7;
+        assert arr1[5] == 1;
+        assert arr1[6] == 2;
+
+        int[] arr2 = original_arr.clone();
+        System.out.println(Arrays.toString(arr2));
+        RotateLeftInPlace(arr2, 8);
+        System.out.println(Arrays.toString(arr2));
+        assert arr2[0] == 2;
+        assert arr2[1] == 3;
+        assert arr2[2] == 4;
+        assert arr2[3] == 5;
+        assert arr2[4] == 6;
+        assert arr2[5] == 7;
+        assert arr2[6] == 1;
 
         int catches = 0;
         try {
-            RotateLeftInPlace(arr, -1);
+            RotateLeftInPlace(original_arr, -1);
         } catch(IllegalArgumentException e) {
             assert e.getMessage().equals(NON_NEGATIVE_K_ERROR_MSG);
             catches++;
         }
         try {
-            RotateLeftInPlace(null, 7);
+            RotateLeftInPlace(null, 2);
         } catch(IllegalArgumentException e) {
             assert e.getMessage().equals(EMPTY_ARRAY_ERROR_MSG);
             catches++;
